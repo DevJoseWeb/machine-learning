d98cd55a644221402b955df4e280e277d4bdcdd8
Junior <web2ajax@gmail.com>
2018-02-26 13:11:03 -0300

    Update classifica_acessos.py

--- capitulo2/classifica_acessos.py
+++ capitulo2/classifica_acessos.py
@@ -1,16 +1,21 @@
+#Importa CVS
 from dados import carregar_acessos
 X, Y = carregar_acessos()
 
+#90 Primeiras linhas para cada um deles
 treino_dados = X[:90]
 treino_marcacoes = Y[:90]
 
+#10% dos que restaram para variável de teste, 9 ultimas linhas
 teste_dados = X[-9:]
 teste_marcacoes = Y[-9:]
 
+#Multinomial Naive Bayes é adequado para classificação com características discretas
 from sklearn.naive_bayes import MultinomialNB
 modelo = MultinomialNB()
 modelo.fit(treino_dados, treino_marcacoes)
 
+#Preveja o Resultado
 resultado = modelo.predict(teste_dados)
 
 diferencas = resultado - teste_marcacoes
